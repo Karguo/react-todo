3458483aa8ac4f74d152b4528d7bf9654f61617b
Karen Kuek <karenkk75@gmail.com>
2019-01-01 21:26:57 +1100

    Updates

---
+++ precache-manifest.2f69257b5a3aefb484fd0f1c4644fa43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5bee33ad84f16ca2cf0f",
+    "url": "/react-todo/static/js/runtime~main.5bee33ad.js"
+  },
+  {
+    "revision": "593fb11d5202aa93bebd",
+    "url": "/react-todo/static/js/main.593fb11d.chunk.js"
+  },
+  {
+    "revision": "9c376d4822a8f1f54887",
+    "url": "/react-todo/static/js/1.9c376d48.chunk.js"
+  },
+  {
+    "revision": "593fb11d5202aa93bebd",
+    "url": "/react-todo/static/css/main.12eeadff.chunk.css"
+  },
+  {
+    "revision": "0e42375c8563915dca64457cbae51c15",
+    "url": "/react-todo/index.html"
+  }
+];
